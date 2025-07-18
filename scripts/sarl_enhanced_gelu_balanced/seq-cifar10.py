a3f105b3461e969dc5bd41d32d273ee4d01b68a2
bachdx-3469 <duong.xuan.bach@sun-asterisk.com>
2025-07-17 22:14:57 -0700

    Exp: Contrast Balanced SARL

--- scripts/sarl_enhanced_gelu_balanced/seq-cifar10.py
+++ scripts/sarl_enhanced_gelu_balanced/seq-cifar10.py
@@ -1,95 +1,123 @@
 #!/usr/bin/env python3
 """
 Enhanced SARL with GELU activation and Balanced Instance Sampling for Sequential CIFAR-10
-This script runs experiments with the sarl_enhanced_gelu_balanced model
+This script defines a grid search for hyperparameters in the sarl_enhanced_gelu_balanced model
 """
-
 import os
 
 best_params = {
-    200: {
-        'idt': 'gelu_balanced_v1_best',
-        'alpha': 0.5,
-        'beta': 1.0,
-        'op_weight': 0.5,
-        'sm_weight': 0.01,
-        'sim_lr': 0.001,
-        'lr': 0.03,
-        'minibatch_size': 32,
-        'batch_size': 32,
-        'n_epochs': 50,
-        'lr_steps': '35 45',
-        'warmup_epochs': 3,
-        'apply_gelu': '1 1 1 1',  # Apply GELU to all layers
-        'num_feats': 512,
-        'use_lr_scheduler': 1,
-        'use_balanced_sampling': 1,
-        'balance_weight': 1.0
-    },
-    500: {
-        'idt': 'gelu_balanced_v1_best',
-        'alpha': 0.2,
-        'beta': 1.0,
-        'op_weight': 0.5,
-        'sm_weight': 0.01,
-        'sim_lr': 0.001,
-        'lr': 0.03,
-        'minibatch_size': 32,
-        'batch_size': 32,
-        'n_epochs': 50,
-        'lr_steps': '35 45',
-        'warmup_epochs': 3,
-        'apply_gelu': '1 1 1 1',  # Apply GELU to all layers
-        'num_feats': 512,
-        'use_lr_scheduler': 1,
-        'use_balanced_sampling': 1,
-        'balance_weight': 1.0
-    },
+    200: [
+        {
+            'idt': f'gelu_balanced_v1_buf200_alpha{a}_beta{b}_op{op}_sm{sm}_lr{lr}_simlr{slr}_bs{bs}_mb{mb}_ep{ep}_warm{w}_gelu{g.replace(" ", "-")}_feat{f}_bw{bw}',
+            'alpha': a,
+            'beta': b,
+            'op_weight': op,
+            'sm_weight': sm,
+            'sim_lr': slr,
+            'lr': lr,
+            'minibatch_size': mb,
+            'batch_size': bs,
+            'n_epochs': ep,
+            'lr_steps': '35 45',  # Changed to comma-separated string
+            'warmup_epochs': w,
+            'apply_gelu': g,
+            'num_feats': f,
+            'use_lr_scheduler': 1,
+            'use_balanced_sampling': 1,
+            'balance_weight': bw
+        }
+        for a in [0.5]
+        for b in [1.0]
+        for op in [0.5, 0.7]
+        for sm in [0.01, 0.05]
+        for slr in [0.001]
+        for lr in [0.03]
+        for mb in [32]
+        for bs in [64]
+        for ep in [50]
+        for w in [5]
+        for g in ['1 1 1 1']
+        for f in [512, 1024]
+        for bw in [1.0, 2.0]
+    ],
+    500: [
+        {
+            'idt': f'gelu_balanced_v1_buf500_alpha{a}_beta{b}_op{op}_sm{sm}_lr{lr}_simlr{slr}_bs{bs}_mb{mb}_ep{ep}_warm{w}_gelu{g.replace(" ", "-")}_feat{f}_bw{bw}',
+            'alpha': a,
+            'beta': b,
+            'op_weight': op,
+            'sm_weight': sm,
+            'sim_lr': slr,
+            'lr': lr,
+            'minibatch_size': mb,
+            'batch_size': bs,
+            'n_epochs': ep,
+            'lr_steps': '35 45',  # Changed to comma-separated string
+            'warmup_epochs': w,
+            'apply_gelu': g,
+            'num_feats': f,
+            'use_lr_scheduler': 1,
+            'use_balanced_sampling': 1,
+            'balance_weight': bw
+        }
+        for a in [0.2]
+        for b in [1.0]
+        for op in [0.5, 0.7]
+        for sm in [0.01, 0.05]
+        for slr in [0.001]
+        for lr in [0.03]
+        for mb in [32]
+        for bs in [64]
+        for ep in [50]
+        for w in [5]
+        for g in ['1 1 1 1']
+        for f in [512, 1024]
+        for bw in [1.0, 2.0]
+    ],
 }
-
 
 lst_seed = [1, 3, 5]
 lst_buffer_size = [200, 500]
 count = 0
 output_dir = "experiments/sarl_enhanced_gelu_balanced"
-save_model = 0  # set to 1 to save the final model
-save_interim = 0  # set to 1 to save intermediate model state and running params
+save_model = 0  # Set to 1 to save the final model
+save_interim = 0  # Set to 1 to save intermediate model state and running params
 device = 'cuda'
 
 # Run balanced sampling experiments
 for seed in lst_seed:
     for buffer_size in lst_buffer_size:
-        params = best_params[buffer_size]
-        exp_id = f"sarl-enhanced-gelu-balanced-cifar10-{buffer_size}-param-{params['idt']}-s-{seed}"
-        job_args = f"python main.py  \
-            --experiment_id {exp_id} \
-            --model sarl_enhanced_gelu_balanced \
-            --dataset seq-cifar10 \
-            --alpha {params['alpha']} \
-            --beta {params['beta']} \
-            --op_weight {params['op_weight']} \
-            --sm_weight {params['sm_weight']} \
-            --sim_lr {params['sim_lr']} \
-            --buffer_size {buffer_size} \
-            --batch_size {params['batch_size']} \
-            --minibatch_size {params['minibatch_size']} \
-            --lr {params['lr']} \
-            --lr_steps {params['lr_steps']} \
-            --n_epochs {params['n_epochs']} \
-            --warmup_epochs {params['warmup_epochs']} \
-            --apply_gelu {params['apply_gelu']} \
-            --num_feats {params['num_feats']} \
-            --use_lr_scheduler {params['use_lr_scheduler']} \
-            --use_balanced_sampling {params['use_balanced_sampling']} \
-            --balance_weight {params['balance_weight']} \
-            --output_dir {output_dir} \
-            --csv_log \
-            --seed {seed} \
-            --device {device} \
-            --save_model {save_model} \
-            --save_interim {save_interim} \
-            "
-        count += 1
-        os.system(job_args)
+        for params in best_params[buffer_size]:
+            exp_id = f"sarl-enhanced-gelu-balanced-cifar10-{buffer_size}-param-{params['idt']}-s-{seed}"
+            job_args = f"python main.py  \
+                --experiment_id \"{exp_id}\" \
+                --model sarl_enhanced_gelu_balanced \
+                --dataset seq-cifar10 \
+                --alpha {params['alpha']} \
+                --beta {params['beta']} \
+                --op_weight {params['op_weight']} \
+                --sm_weight {params['sm_weight']} \
+                --sim_lr {params['sim_lr']} \
+                --buffer_size {buffer_size} \
+                --batch_size {params['batch_size']} \
+                --minibatch_size {params['minibatch_size']} \
+                --lr {params['lr']} \
+                --lr_steps \{params['lr_steps']}\ \
+                --n_epochs {params['n_epochs']} \
+                --warmup_epochs {params['warmup_epochs']} \
+                --apply_gelu \{params['apply_gelu']}\ \
+                --num_feats {params['num_feats']} \
+                --use_lr_scheduler {params['use_lr_scheduler']} \
+                --use_balanced_sampling {params['use_balanced_sampling']} \
+                --balance_weight {params['balance_weight']} \
+                --output_dir \"{output_dir}\" \
+                --csv_log \
+                --seed {seed} \
+                --device {device} \
+                --save_model {save_model} \
+                --save_interim {save_interim} \
+                "
+            count += 1
+            os.system(job_args)
 
-print('%s jobs counted' % count) 
+print(f'{count} jobs counted')
